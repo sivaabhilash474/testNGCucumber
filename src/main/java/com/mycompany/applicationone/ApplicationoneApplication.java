e1ed68308a9f3bee24599b5410fba53295a9f59b
sivaabhilash474 <sivaabhilash474@gmail.com>
2025-02-24 20:56:27 -0500

    refactored

--- src/main/java/com/mycompany/applicationone/ApplicationoneApplication.java
+++ src/main/java/com/mycompany/applicationone/ApplicationoneApplication.java
@@ -2,8 +2,12 @@ package com.mycompany.applicationone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.mycompany.applicationone")
+@EnableJpaRepositories(basePackages = "com.mycompany.applicationone.repository")
+@EntityScan(basePackages = "com.mycompany.applicationone.entity")
 public class ApplicationoneApplication {
 
 	public static void main(String[] args) {
