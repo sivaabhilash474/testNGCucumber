c2f65196989de04d26408c8a903273d0e2f79a6f
sivaabhilash474 <sivaabhilash474@gmail.com>
2025-02-24 21:00:09 -0500

    updated

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO TBL_PLAYERS (first_name, last_name, email) VALUES
+  ('MS', 'Dhoni', 'msd@cricket.com'),
+  ('Rahul', 'Dravid', 'rahul.dravid@cricket.com'),
+  ('Viral', 'kohli', 'virat.kohli@cricket.com');
+  
+  
+INSERT INTO CITY VALUES (11, 'Delhi', 110001);    
+INSERT INTO CITY VALUES (12, 'Kanpur', 208001);    
+INSERT INTO CITY VALUES (13, 'Lucknow', 226001);    
